16da461db1c5febe4a7cc8750ea635cf69aa1814
Paul Cowan <paul@custardsource.com>
2021-12-12 19:21:50 +1100

    Add CPA point calculation

--- server.js
+++ server.js
@@ -44,6 +44,43 @@ function CPA(speed1,course1,speed2,course2,range,bearing)
   return [cpa, 60*(-b/(2*a))];
 }
 
+function distanceAfterSeconds(speedKts, timeSeconds) {
+  var speedMs = speedKts * 0.51444444444444;
+  var distanceM = speedMs * timeSeconds;
+  return distanceM;
+}
+
+// From https://stackoverflow.com/questions/19352921/how-to-use-direction-angle-and-speed-to-calculate-next-times-latitude-and-longi
+function destinationPoint(lat, lon, distance, bearing) {
+     var radius = 6371e3; // (Mean) radius of earth
+
+     var toRadians = function(v) { return v * Math.PI / 180; };
+     var toDegrees = function(v) { return v * 180 / Math.PI; };
+
+     // sinφ2 = sinφ1·cosδ + cosφ1·sinδ·cosθ
+     // tanΔλ = sinθ·sinδ·cosφ1 / cosδ−sinφ1·sinφ2
+     // see mathforum.org/library/drmath/view/52049.html for derivation
+
+     var δ = Number(distance) / radius; // angular distance in radians
+     var θ = toRadians(Number(bearing));
+
+     var φ1 = toRadians(Number(lat));
+     var λ1 = toRadians(Number(lon));
+
+     var sinφ1 = Math.sin(φ1), cosφ1 = Math.cos(φ1);
+     var sinδ = Math.sin(δ), cosδ = Math.cos(δ);
+     var sinθ = Math.sin(θ), cosθ = Math.cos(θ);
+
+     var sinφ2 = sinφ1*cosδ + cosφ1*sinδ*cosθ;
+     var φ2 = Math.asin(sinφ2);
+     var y = sinθ * sinδ * cosφ1;
+     var x = cosδ - sinφ1 * sinφ2;
+     var λ2 = λ1 + Math.atan2(y, x);
+
+     return [toDegrees(φ2), (toDegrees(λ2)+540)%360-180]; // normalise to −180..+180°
+  }
+
+
 
 var imgCache = {}
 
@@ -76,6 +113,9 @@ function pollUpdate() {
          e['cpa_km'] = cpa[0] * 1.852
          e['cpa_secs'] = cpa[1] * 60
          e['cpa_altitude_estimate'] = e['alt_baro'] + (cpa[1] * e['baro_rate'])
+         var cpaPoint = destinationPoint(e['lat'], e['lon'], distanceAfterSeconds(e['gs'], e['cpa_secs']) ,  e['track']);
+         e['cpa_lat'] = cpaPoint[0]
+         e['cpa_lon'] = cpaPoint[1]
 	}
 	if (e['hex'] in imgCache) {
 	  e['image'] = imgCache[e['hex']]
